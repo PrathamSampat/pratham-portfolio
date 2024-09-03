f645f734cd0025ee54f31964cbf9be7ce37dcf79
PrathamSampat <prathamsampat2002.ps@gmail.com>
2024-09-03 17:06:28 +0530

    corrected photo

--- src/components/ui/Photo.jsx
+++ src/components/ui/Photo.jsx
@@ -1,41 +1,52 @@
 "use client";
-
-import Image from 'next/image';
-import { easeIn, easeInOut, motion } from 'framer-motion';
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 const Photo = () => {
     return (
-        <div className='w-full h-full relative'>
+        <div className="w-full h-full relative">
             <motion.div
                 initial={{ opacity: 0 }}
                 animate={{
                     opacity: 1,
                     transition: {
-                        delay: 2,
+                        delay: 1.3,
                         duration: 0.4,
-                        ease: easeIn
-                    }
-                }}>
+                        ease: "easeIn",
+                    },
+                }}
+            >
                 <motion.div
+                    className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
                     initial={{ opacity: 0 }}
                     animate={{
                         opacity: 1,
                         transition: {
-                            delay: 2.4,
+                            delay: 1.7,
                             duration: 0.4,
-                            ease: easeInOut
-                        }
+                            ease: "easeInOut",
+                        },
                     }}
-                    className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]
-                    mix-blend-lighten'>
-                    <Image src="/photo.jpg" priority quality={100} fill
-                        alt='' className='object-contain' />
+                >
+                    <Image
+                        src="/photo.jpg"
+                        priority
+                        quality={100}
+                        fill
+                        alt="profile picture"
+                        className="object-contain"
+                    ></Image>
                 </motion.div>
-                <motion.svg classname="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
-                    fill="transarent"
+
+                {/* circle */}
+                <motion.svg
+                    className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+                    fill="transparent"
                     viewBox="0 0 506 506"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <motion.circle cx="253"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <motion.circle
+                        cx="253"
                         cy="253"
                         r="250"
                         stroke="#00ff99"
@@ -45,17 +56,18 @@ const Photo = () => {
                         initial={{ strokeDasharray: "24 10 0 0" }}
                         animate={{
                             strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
-                            rotate: [120, 360]
+                            rotate: [120, 360],
                         }}
                         transition={{
                             duration: 20,
                             repeat: Infinity,
-                            repeatType: "reverse"
-                        }} />
+                            repeatType: "reverse",
+                        }}
+                    ></motion.circle>
                 </motion.svg>
             </motion.div>
-        </div >
-    )
-}
+        </div>
+    );
+};
 
-export default Photo
+export default Photo;
